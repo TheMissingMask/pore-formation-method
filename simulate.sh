c33d236150b1ecdb8c76a0627f193f60d215c161
Victor Everett <39962129+TheMissingMask@users.noreply.github.com>
2020-09-11 13:58:45 +0100

    Update simulate.sh

--- simulate.sh
+++ simulate.sh
@@ -4,8 +4,8 @@ cd em
 gmx grompp -f ../initial/mdp/em.mdp -p ../initial/topol.top -c ../initial/membrane.gro -o em.tpr
 gmx mdrun -deffnm em -v
 cd ../eq
-gmx grompp -f ../initial/mdp/eq.mdp -n ../em/index.ndx -c ../em/em.gro -p ../initial/topol.top -r ../initial/ref.gro -o eq.tpr
+gmx grompp -f ../initial/mdp/eq.mdp -c ../em/em.gro -p ../initial/topol.top -r ../initial/ref.gro -o eq.tpr
 gmx mdrun -deffnm eq -v -dlb yes 
 cd ../md
-gmx grompp -f ../initial/mdp/md.mdp -n ../em/index.ndx -c ../eq/eq.gro -p ../initial/topol.top -r ../initial/ref.gro -o md.tpr
+gmx grompp -f ../initial/mdp/md.mdp -c ../eq/eq.gro -p ../initial/topol.top -r ../initial/ref.gro -o md.tpr
 gmx mdrun -deffnm md -v -dlb yes 
